39693475e5f0979bc07966575cb445b7e2b6388b
Anyona-00 <125492744+Anyona-00@users.noreply.github.com>
2023-12-12 14:42:02 +0300

    wow react

---
+++ index.js
@@ -0,0 +1,16 @@
+const page = (
+    <div>
+        <img src="./logo.png" alt="logo" width ="40px" height="40px"/>
+        <h1>Fun Facts About React</h1>
+
+        <ul>
+            <li>Was released in 2013</li>
+            <li>Was originaly created by Jotrdane Walke</li>
+            <li>Has over 100k stars on Github</li>
+            <li>Is maintained by facebook</li>
+            <li>Powers thuosands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
+)
+
+ReactDOM.render(page,document.getElementById("root"))
