fe815549c1122491e2cd8dc8629547317b8ec197
Anyona-00 <125492744+Anyona-00@users.noreply.github.com>
2023-12-13 06:49:38 +0300

    react components

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-const page = (
-    <div>
-        <img src="./logo.png" alt="logo" width ="40px" height="40px"/>
-        <h1>Fun Facts About React</h1>
 
+function Header() {
+    return (
+        <header>
+        <nav className="nav">
+        <img src="./logo.png" alt="logo" width ="40px" height="40px"  className="logo"/>
+        <ul className="nav-items">
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contacts</li>
+        </ul>
+        </nav>
+    </header>
+    )}
+
+
+
+    function MainContent() {
+        return (
+            <div>
+            <h1>Fun Facts About React</h1>
         <ul>
             <li>Was released in 2013</li>
             <li>Was originaly created by Jotrdane Walke</li>
@@ -10,7 +26,47 @@ const page = (
             <li>Is maintained by facebook</li>
             <li>Powers thuosands of enterprise apps, including mobile apps</li>
         </ul>
-    </div>
-)
+        </div>
+        )}
 
-ReactDOM.render(page,document.getElementById("root"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        function Footer() {
+            return (
+                <footer>
+                    <p>&#169; 2023 Anyona develpment. All Right Reserved</p>
+                </footer>
+            )}
+
+
+
+
+
+
+
+function Page() {
+    return (
+        <div>
+            <Header />
+           
+            <MainContent />
+
+            <Footer />
+        </div>
+    )
+} 
+
+ReactDOM.render(<Page />,document.getElementById("root"))
