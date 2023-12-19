fbef6e88a430d2ff28ce7351be076f2d3eed72be
Anyona-00 <125492744+Anyona-00@users.noreply.github.com>
2023-12-19 10:50:07 +0300

    Fix line endings

---
+++ vite/src/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer>
+            <p className="footer">&#169; 2023 Anyona develpment. All Right Reserved</p>
+        </footer>
+    )}
---
+++ vite/src/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+ function Header() {
+    return (
+        <header>
+        <nav className="nav">
+        <img src="./logo.png" alt="logo"   className="logo"/>
+        <ul className="nav-items">
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contacts</li>
+        </ul>
+        </nav>
+    </header>
+    )}
+
+    export default Header
