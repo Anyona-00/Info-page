0bf8fef1bb879d87b537549c29c518a3e59768f4
Anyona-00 <125492744+Anyona-00@users.noreply.github.com>
2023-12-19 10:53:17 +0300

    update react

--- Footer.js
+++
@@ -1,7 +0,0 @@
-
-export default function Footer() {
-    return (
-        <footer>
-            <p className="footer">&#169; 2023 Anyona develpment. All Right Reserved</p>
-        </footer>
-    )}
